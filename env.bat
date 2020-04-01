5689461fcaf489e396cbf9c464b2fad792e7daa9
imotyashok <imotyashok@luc.edu>
2020-04-01 09:18:51 -0500

    added files for a very basic outline of the site

---
+++ env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=quiz_app/quiz_app.py
+set FLASK_DEBUG=1
+REM set any mongo connect (or even sql connect strings if you want)
---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FLASK_APP=quiz_app/quiz_app.py
+export FLASK_DEBUG=1
