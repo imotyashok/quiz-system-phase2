71f223a5bdb92e0196a4f0f04d14eb7edd884587
imotyashok <=imotyashok@luc.edu>
2020-04-15 20:39:52 -0500

    added images to make the site prettier, working on saving the quiz file

--- quiz_app/routes.py
+++ quiz_app/routes.py
@@ -55,6 +55,51 @@ def createquiz(mc_num, tf_num, fib_num):
     # TO-DO: figure out what needs to be done once the final form gets submitted  
     if form.is_submitted():
         #    do stuff...
+        result = request.form
+
+        # NOTE: The results are stored in a dictionary that follows this format:
+        # print(result) --> 
+        # ImmutableMultiDict([('mc-question', 'What color is a banana?'), ('mc-c1', 'Purple'), 
+        #                      ('mc-c2', 'Green'), ('mc-c3', 'Yellow'), ('mc-c4', 'Red'), 
+        #                      ('mc-answer', 'c'), ('tf-question', 'Computer mice are mammals. '), 
+        #                      ('tf-answer', 'f'), ('fib-question', 'The best OS is'), 
+        #                      ('fib-answer', 'Linux'), ('submit', 'Save Quiz')])
+        #
+        # Some problems with questions of the same type... ex:
+        # ImmutableMultiDict([('tf-question', 'Computer mice are mammals. '), 
+        #                     ('tf-question', 'Python is both a programming language and a snake'), 
+        #                     ('tf-question', 'Some flowers are yellow. '), ('tf-answer', 'f'), 
+        #                     ('tf-answer', 't'), ('tf-answer', 't'), ('submit', 'Save Quiz')])
+
+        mc_q_list = request.form.getlist('mc-question')
+        mc_c1_list = request.form.getlist('mc-c1')
+        mc_c2_list = request.form.getlist('mc-c2') 
+        mc_c3_list = request.form.getlist('mc-c3') 
+        mc_c4_list = request.form.getlist('mc-c4')
+        mc_ans_list = request.form.getlist('mc-answer') 
+
+        tf_q_list =  request.form.getlist('tf-question')
+        tf_ans_list = request.form.getlist('tf-answer')
+
+        fib_q_list = request.form.getlist('fib-question')
+        fib_ans_list = request.form.getlist('fib-answer')
+
+        if mc_q_list:
+            for q in range(len(mc_q_list)):
+                print("Question %d:" %(q+1))
+                print(mc_q_list[q], mc_c1_list[q], mc_c2_list[q], mc_c3_list[q], mc_c4_list[q], mc_ans_list[q])
+            print("\n")
+        if tf_q_list:
+            for q in range(len(tf_q_list)):
+                print("Question %d:" %(q+1))
+                print(tf_q_list[q], tf_ans_list[q])
+            print("\n")
+        if fib_q_list:
+            for q in range(len(fib_q_list)):
+                print("Question %d:" %(q+1))
+                print(fib_q_list[q], fib_ans_list[q])
+            print("\n")
+
         return redirect(url_for('home')) # redirect to home for now lol
         
     # NOTE: form.mc accesses the MultipleChoiceForm, form.tf accesses the TrueFalseForm, and 
