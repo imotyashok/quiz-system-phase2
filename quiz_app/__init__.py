a2cb7cbf3e67cdedf2175dcd7ec1a3a0f6bfdf97
ErikCG <50557869+ErikCG@users.noreply.github.com>
2020-04-20 15:30:52 -0400

    Changed and added code to use SocketIO

--- quiz_app/__init__.py
+++ quiz_app/__init__.py
@@ -1,14 +1,18 @@
 # This file initializes our actual Flask application 
 
 from flask import Flask
+from flask_socketio import SocketIO
 
 # creating our app
-app = Flask(__name__, static_url_path="/static") 
+app = Flask(__name__, static_url_path="/static")
 
 # assigning secret key in order to encrypt our cookies and safely send them to the browser
-app.config['SECRET_KEY'] = '77a2c153f819019e8657ceea0848de0d' 
-app.config["QUIZ_FILE"] = "C:/Users/iryna/quiz-system-phase2/quiz_app/static" 
+app.config['SECRET_KEY'] = '77a2c153f819019e8657ceea0848de0d'
+app.config["QUIZ_FILE"] = "C:/Users/iryna/quiz-system-phase2/quiz_app/static"
 
-# importing our routes.py and forms.py files 
+# creating socketio instance
+socketio = SocketIO(app)
+
+# importing our routes.py and forms.py files
 from quiz_app import routes
 from quiz_app import forms
--- quiz_app/routes.py
+++ quiz_app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, request, redirect, url_for, send_file, send_from_directory, flash 
+from flask import render_template, request, redirect, url_for, send_file, send_from_directory, flash
+from flask_socketio import SocketIO
 from quiz_app import app
+from quiz_app import socketio
 from quiz_app.forms import *
 
 from datetime import datetime
@@ -8,80 +10,84 @@ import csv
 
 @app.route("/")
 @app.route("/home")
-
 def home():
     return render_template('home.html', title='Home Page')
+
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About Page')
-    
+
+
 @app.route("/makequiz")
 def makequiz():
     return render_template('makequiz.html', title='Make a Quiz')
-    
+
+
 @app.route("/takequiz")
 def takequiz():
     return render_template('takequiz.html', title='Take a Quiz')
-    
-@app.route("/questiontypes", methods=['GET', 'POST']) 
+
+
+@app.route("/questiontypes", methods=['GET', 'POST'])
 def questiontypes():
-    # This function gets and returns the results of the QuestionTypesForm, which asks users 
+    # This function gets and returns the results of the QuestionTypesForm, which asks users
     # how many questions they want of each question type (multiple choice, true false, and matching)
 
-    form = QuestionTypesForm() # we create our form object 
+    form = QuestionTypesForm()  # we create our form object
     if form.is_submitted():
         # Get the results of the form; these are stored as a dictionary, with the key
         # being the variable name of each field on our form (ex. for this one, the keys
-        # would be 'multiple_choice', 'true_false', 'matching', and 'submit') and the 
-        # values being the actual number values the user inputs for those fields 
+        # would be 'multiple_choice', 'true_false', 'matching', and 'submit') and the
+        # values being the actual number values the user inputs for those fields
         result = request.form
 
-        # Extracting the values the user submitted in the form 
+        # Extracting the values the user submitted in the form
         mc_num = result['multiple_choice']
         tf_num = result['true_false']
         fib_num = result['fill_in_blank']
 
         # Now we pass the numbers of question types the user entered to the createquiz page
-        return redirect(url_for("createquiz", mc_num=mc_num, tf_num=tf_num, fib_num=fib_num))   
-        
+        return redirect(url_for("createquiz", mc_num=mc_num, tf_num=tf_num, fib_num=fib_num))
+
     return render_template('questiontypes.html', title='Begin Building Your Quiz', form=form)
 
-@app.route("/createquiz/<mc_num>_<tf_num>_<fib_num>", methods=['GET', 'POST']) 
-def createquiz(mc_num, tf_num, fib_num):
-    # Now we can use the number of each question type the user wanted in order to 
-    # generate the form that actually lets them build the quiz 
 
-    # Converting these numbers to integer type: 
+@app.route("/createquiz/<mc_num>_<tf_num>_<fib_num>", methods=['GET', 'POST'])
+def createquiz(mc_num, tf_num, fib_num):
+    # Now we can use the number of each question type the user wanted in order to
+    # generate the form that actually lets them build the quiz
+
+    # Converting these numbers to integer type:
     mc_num, tf_num, fib_num = int(mc_num), int(tf_num), int(fib_num)
-    
-    form = QuizBuilderForm() # Generating the actual quiz building form 
-    # TO-DO: figure out what needs to be done once the final form gets submitted  
+
+    form = QuizBuilderForm()  # Generating the actual quiz building form
+    # TO-DO: figure out what needs to be done once the final form gets submitted
     if form.is_submitted():
         #    do stuff...
 
         return create_file(form)
 
-       # return redirect(url_for('home')) # redirect to home for now lol
+    # return redirect(url_for('home')) # redirect to home for now lol
 
-        
-    # NOTE: form.mc accesses the MultipleChoiceForm, form.tf accesses the TrueFalseForm, and 
-    #       form.match accesses MatchingForm; to access individual fields from each form in your HTML, just 
+    # NOTE: form.mc accesses the MultipleChoiceForm, form.tf accesses the TrueFalseForm, and
+    #       form.match accesses MatchingForm; to access individual fields from each form in your HTML, just
     #       add another method call;
-    #           ex. To access the "question" field of the MultipleChoiceForm, you'd do 'form.mc.question' 
+    #           ex. To access the "question" field of the MultipleChoiceForm, you'd do 'form.mc.question'
 
-    return render_template('createquiz.html', title='Create Quiz Results', form=form, 
-                            mc_num=mc_num, tf_num=tf_num, fib_num=fib_num)
-    
+    return render_template('createquiz.html', title='Create Quiz Results', form=form,
+                           mc_num=mc_num, tf_num=tf_num, fib_num=fib_num)
+
+
 def create_file(form):
     mc_q_list = request.form.getlist('mc-question')
     mc_c1_list = request.form.getlist('mc-c1')
-    mc_c2_list = request.form.getlist('mc-c2') 
-    mc_c3_list = request.form.getlist('mc-c3') 
+    mc_c2_list = request.form.getlist('mc-c2')
+    mc_c3_list = request.form.getlist('mc-c3')
     mc_c4_list = request.form.getlist('mc-c4')
-    mc_ans_list = request.form.getlist('mc-answer') 
+    mc_ans_list = request.form.getlist('mc-answer')
 
-    tf_q_list =  request.form.getlist('tf-question')
+    tf_q_list = request.form.getlist('tf-question')
     tf_ans_list = request.form.getlist('tf-answer')
 
     fib_q_list = request.form.getlist('fib-question')
@@ -90,14 +96,14 @@ def create_file(form):
     q_dict = dict()
 
     # NOTE: 'q_dict' is a dictionary of questions that follows the following format:
-        #    question-type_question-number : ('question', 'option1', 'option2', 'answer')
+    #    question-type_question-number : ('question', 'option1', 'option2', 'answer')
 
-        # Here is an example of an output from the form: 
-        # { 'mc_q1': ('What color is a banana?', 'Purple', 'Green', 'Yellow', 'Red', 'c'), 
-        #   'mc_q2': ('How many hands does a human have?', '0', '2', '3', '4', 'b'), 
-        #   'tf_q1': ('Computer mice are mammals. ', 'f'), 'tf_q2': ('Water is a liquid. ', 't'), 
-        #   'fib_q1': ('The best OS is', 'Linux'), 'fib_q2': ('My name is ', 'Slim Shady')}
-        # I'm not even sure this dictionary will be useful yet, but hey, it exists! 
+    # Here is an example of an output from the form:
+    # { 'mc_q1': ('What color is a banana?', 'Purple', 'Green', 'Yellow', 'Red', 'c'),
+    #   'mc_q2': ('How many hands does a human have?', '0', '2', '3', '4', 'b'),
+    #   'tf_q1': ('Computer mice are mammals. ', 'f'), 'tf_q2': ('Water is a liquid. ', 't'),
+    #   'fib_q1': ('The best OS is', 'Linux'), 'fib_q2': ('My name is ', 'Slim Shady')}
+    # I'm not even sure this dictionary will be useful yet, but hey, it exists!
 
     # TODO: implement a function that takes this q_dict and downloads this file to the user's computer
     # in some sort of format (CSV?)
@@ -106,55 +112,76 @@ def create_file(form):
 
         print("writing csv file")
 
-        quiz_writer = csv.writer(quiz_file, delimiter=';', quotechar='"',quoting=csv.QUOTE_MINIMAL)
+        quiz_writer = csv.writer(quiz_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
         if mc_q_list:
             for q in range(len(mc_q_list)):
-                quiz_writer.writerow(["mc_q%d"%(q+1), mc_q_list[q], mc_c1_list[q], mc_c2_list[q], mc_c3_list[q], mc_c4_list[q], mc_ans_list[q]])
+                quiz_writer.writerow(
+                    ["mc_q%d" % (q + 1), mc_q_list[q], mc_c1_list[q], mc_c2_list[q], mc_c3_list[q], mc_c4_list[q],
+                     mc_ans_list[q]])
 
         if tf_q_list:
             for q in range(len(tf_q_list)):
-                quiz_writer.writerow(["tf_q%d"%(q+1), tf_q_list[q], tf_ans_list[q]])
+                quiz_writer.writerow(["tf_q%d" % (q + 1), tf_q_list[q], tf_ans_list[q]])
 
         if fib_q_list:
             for q in range(len(fib_q_list)):
-                quiz_writer.writerow(["fib_q%d"%(q+1), fib_q_list[q], fib_ans_list[q]])
-        
-    curr_time = datetime.now().strftime("%c")    
+                quiz_writer.writerow(["fib_q%d" % (q + 1), fib_q_list[q], fib_ans_list[q]])
+
+    curr_time = datetime.now().strftime("%c")
     print(curr_time)
 
     FILEPATH = 'static/quiz.csv'
 
-    #return send_from_directory(app.config["QUIZ_FILE"], "quiz_%s.csv"%(curr_time), as_attachment=True)
-   
-   # THE NEXT LINE IS GOOD 
-   # return send_file('static/quiz.csv', mimetype='text/csv', attachment_filename='quiz_04152020.csv', as_attachment=True)
+    # return send_from_directory(app.config["QUIZ_FILE"], "quiz_%s.csv"%(curr_time), as_attachment=True)
+
+    # THE NEXT LINE IS GOOD
+    # return send_file('static/quiz.csv', mimetype='text/csv', attachment_filename='quiz_04152020.csv', as_attachment=True)
 
     return redirect(url_for('download_quiz'))
-   # return redirect(url_for('home'))
+
+
+# return redirect(url_for('home'))
 
 @app.route("/download_quiz")
 def download_quiz():
     return render_template('download_quiz.html')
 
+
 @app.route("/return_file")
 def return_file():
     FILEPATH = 'static/quiz.csv'
-    #csv_f = open('static/quiz.csv', 'r')
-    #returnfile = csv_f.read().encode('latin-1')
-    #csv_f.close()
+    # csv_f = open('static/quiz.csv', 'r')
+    # returnfile = csv_f.read().encode('latin-1')
+    # csv_f.close()
 
-    #return Response(returnfile, mimetype="text/csv", headers={"Content-disposition":"attachment; filename=quiz_test.csv"})
+    # return Response(returnfile, mimetype="text/csv", headers={"Content-disposition":"attachment; filename=quiz_test.csv"})
 
-    #return send_file('static/quiz.csv', mimetype='text/csv', attachment_filename='quiz_04152020.csv')
-    
+    # return send_file('static/quiz.csv', mimetype='text/csv', attachment_filename='quiz_04152020.csv')
+
     # return send_from_directory('C:/Users/iryna/quiz-system-phase2/quiz_app/static/flower_branch.png', 'branch.png', as_attachment=True, cache_timeout=0)
-    
+
     curr_time = datetime.now().strftime("%m-%d-%Y_%H-%M")
-    attach_name = 'quiz_'+curr_time+'.csv'
+    attach_name = 'quiz_' + curr_time + '.csv'
 
     print(attach_name)
 
     flash("Your quiz has been downloaded!", 'success')
 
-    return send_file(FILEPATH, mimetype='text/csv', attachment_filename=attach_name, as_attachment=True, cache_timeout=1)
+    return send_file(FILEPATH, mimetype='text/csv', attachment_filename=attach_name, as_attachment=True,
+                     cache_timeout=1)
+
+
+@app.route("/host_quiz")
+def sessions():
+    return render_template('host_quiz.html')
+
+
+def scoreReceived(methods=['GET', 'POST']):
+    print('score was recieved')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=scoreReceived)
